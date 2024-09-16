7745734ef2b35895e58b07db967a8c0c3c9b47aa
mrPefr <persson.fredric@gmail.com>
2024-09-16 09:56:59 +0200

    Boiler Plate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App(){
+
+    return (
+       <>
+       
+        <h2>React App with parcel</h2>
+        <h3>Not so hard as it seems...</h3>
+        <h5>Final test</h5>
+       
+       </>
+    )
+}
